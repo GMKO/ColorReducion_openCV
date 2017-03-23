0b4d11d99d0e067f90e4d272be0f98dbc9784188
Bogdan <bogdan_stefan.manea@cti.pub.ro>
2017-03-23 02:09:26 +0200

    Added methods to process vetors.

--- opencvProj/opencv_test.cpp
+++ opencvProj/opencv_test.cpp
@@ -5,22 +5,81 @@
 using namespace cv;
 using namespace std;
 
-Mat computeInput(Mat input) {
+uchar vectModule(double alpha, double beta, double gamma, Vec3b vect) {
+	return	alpha*vect.val[0] 
+			+ beta*vect.val[1] 
+			+ gamma*vect.val[2];
+}
+
+uchar virtualVectModule(double alpha, double beta, double gamma, Vec3b vect1, Vec3b vect2) {
+	return	alpha*abs(vect1.val[0] - vect2.val[0])
+			+ beta*abs(vect1.val[1] - vect2.val[1])
+			+ gamma*abs(vect1.val[2] - vect2.val[2]);
+}
+
+int vectRelation(double alpha, double beta, double gamma, Vec3b vect1, Vec3b vect2) {
+	if (vectModule(alpha, beta, gamma, vect1) < vectModule(alpha, beta, gamma, vect2)) {
+		return 1;
+	}
+	
+	if (vectModule(alpha, beta, gamma, vect1) == vectModule(alpha, beta, gamma, vect2)) {
+		if (vect1.val[0] < vect2.val[0])
+			return 1;
+
+		if (vect1.val[0] == vect2.val[0]) {
+			if (vect1.val[1] < vect2.val[1])
+				return 1;
+		}
+
+		if (vect1.val[0] == vect2.val[0]) {
+			if (vect1.val[1] == vect2.val[1]) {
+				if (vect1.val[2] <= vect2.val[2])
+					return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+uchar optimumModuloDifference(double alpha, double beta, double gamma, Vec3b vect1, Vec3b vect2) {
+	if (vectRelation(alpha, beta, gamma, vect1, vect2) == 1)
+		return	(-virtualVectModule(alpha, beta, gamma, vect1, vect2) 
+				+ vectModule(alpha, beta, gamma, vect1) 
+				- vectModule(alpha, beta, gamma, vect2)) / 2;
+	else
+		return	(virtualVectModule(alpha, beta, gamma, vect1, vect2)
+				- vectModule(alpha, beta, gamma, vect1)
+				+ vectModule(alpha, beta, gamma, vect2)) / 2;
+}
+
+Mat computeInput(double alpha, double beta, double gamma, Mat input) {
 	
 	Mat output = Mat(input.rows, input.cols, CV_8UC1, double(0));
 	int i, j;
+	
 	for (i = 0; i<input.rows; i++)
 		for (j = 0; j<input.cols; j++)
-		{
-			Vec3b color = input.at<Vec3b>(i, j);
-			uchar blue = color.val[0];
-			uchar green = color.val[1];
-			uchar red = color.val[2];
+			output.at<uchar>(i, j) = vectModule(alpha, beta, gamma, input.at<Vec3b>(i, j));
 
-			output.at<uchar>(i, j) = (red + green + blue) / 3;
-		}
+	return output;
+}
 
-		return output;
+Mat computeBV(double alpha, double beta, double gamma, Mat input) {
+
+	Mat output = Mat(input.rows, input.cols, CV_8UC1, double(0));
+	int i, j;
+
+	for (i = 1; i < input.rows; i++) {
+		for (j = 0; j < input.cols; j++)
+			output.at<uchar>(i, j) = optimumModuloDifference(alpha, beta, gamma, input.at<Vec3b>(i - 1, j), input.at<Vec3b>(i, j));
+	}
+		
+	for (j = 0; j < input.cols; j++) {
+		
+	}
+
+	return output;
 }
 
 void dispResults(Mat input, char* inputName, Mat output, char* outputName) {
@@ -41,6 +100,9 @@ void dispResults(Mat input, char* inputName, Mat output, char* outputName) {
 
 int main(int argc, char** argv)
 {
+	double alpha = 0.33;
+	double beta = 0.33;
+	double gamma = 0.33;
 	char* inputName = argv[1];
 	Mat input = imread(inputName, 1);
 
@@ -50,10 +112,10 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	Mat G = computeInput(input);
-	Mat A = computeInput(input);
+	Mat G = computeInput(alpha, beta, gamma, input);
+	Mat A = G;
 
-	dispResults(input, inputName, A, "Output.jpg");
+	//dispResults(input, inputName, A, "Output.jpg");
 
 	return 0;
 }
